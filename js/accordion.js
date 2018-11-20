c0ceef542904160af6779da52b7b90f37025f283
kostryukovd <kostryukovd@gmail.com>
2018-11-20 19:58:33 +0300

    c2

---
+++ js/accordion.js
@@ -0,0 +1,23 @@
+/*$('arrow-down').each(function(index){
+	$(this).addEventListener("click",function() {
+
+		$(this).css("display","block");
+
+	});
+});*/
+
+var acc = document.getElementsByClassName("arrow-down");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("arrow-down--rotated");
+        var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+    });
+}
+
---
+++ js/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+   
+    nav:true,
+    items:1,
+    navText:['<img src = "img/arrow_prev.svg">','<img src = "img/arrow_next.svg">']
+});
+
+$('.card-detail__carousel').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery:{enabled:true}
+  // other options
+});
+
+});
+
+
