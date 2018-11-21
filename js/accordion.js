f2926fd0cd73f01d772d6a3067994d15e41c3ade
kostryukovd <kostryukovd@gmail.com>
2018-11-21 20:01:42 +0300

    desktop

--- js/accordion.js
+++ js/accordion.js
@@ -11,7 +11,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        this.classList.toggle("arrow-down--rotated");
+        this.classList.toggle("arrow-down_rotated");
         var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
